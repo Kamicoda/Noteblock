825afb1a7f6523fcf892366f56d17a8c6c3421c5
EC2 Default User <ec2-user@ip-172-31-0-20.eu-west-1.compute.internal>
2019-11-07 21:57:58 +0000

    added readme and removed useless content

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Initial log:
+this website is for notes it saves your notes to local storage so you can open them up at any time
+
+initialy tere was meant to be more to this site however i ignored KISS and ended up recreatingt his site multiple times (lesson learned)
+there will be more added at some point in the future i wil return to this app and do what i originaly intended to do
+
+updates and bugs:
+
+
+. original design was 4 buttons in the center of the screen (redesigned)
+. storage bug (fixed)
+. creating mutiple instences was causing the storage to incorrenctly save ("new" feature removed)
+. notes were bieng saved across all the "edit" divs (fixed)
+. checkboxes not suplaying the correct class (found bug working on it)
+. games button removed due to zero content
+. links button removed due to zero content
+
+
+Summery:
+
+this website is created as a milestone project (little bit too ambitions)
+with a major change in timescales at home ive decided to submit this in it's current state as i am here to learn above all else
+of which i have learned alot through multiple itterations of this site
--- assets/js/script1.js
+++ assets/js/script1.js
@@ -2,13 +2,13 @@
 $(document).ready(function() {
 
     $('#aboutSec').toggle();
-    $('#notesSec').show();
+    $('#notesSec').toggle();
     $('#gamesSec').toggle();
     $('#linksSec').toggle();
     $('.fa-check-square').toggle();
-    
+
     //$('#item').toggle();
-   // $('#item1').toggle();
+    //$('#item1').toggle();
     //$('#item2').toggle();
 
     $('#aboutNav').on('click', function() {
@@ -36,61 +36,61 @@ $(document).ready(function() {
         $('.fa-square').toggle();
         $('.fa-check-square').toggle();
     });
-    
-     $('#checkBox1').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox2').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox3').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox4').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox5').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox6').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox7').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox8').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
-     $('#checkBox9').on('click', function() {
-        $('.fa-square').toggle();
-        $('.fa-check-square').toggle();
-    });
-    
 
-   // $('#new').on('click', function() {
-      //  count += 1;
-      //  $('#item' + count).show()
-        //$('#editList').append(`<p id="edit` + count + `" contenteditable="true" class="editableP"></p><button type="button " onclick="save`+ count +`()" class="saveButton">Save
-                       // <i class="fas fa-file-download"></i></button>`);
+    $('#checkBox1').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
 
-   // });
+    $('#checkBox2').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox3').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox4').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox5').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox6').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox7').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox8').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+    $('#checkBox9').on('click', function() {
+        $('.fa-square').toggle();
+        $('.fa-check-square').toggle();
+    });
+
+
+    // $('#new').on('click', function() {
+    //  count += 1;
+    //  $('#item' + count).show()
+    //$('#editList').append(`<p id="edit` + count + `" contenteditable="true" class="editableP"></p><button type="button " onclick="save`+ count +`()" class="saveButton">Save
+    // <i class="fas fa-file-download"></i></button>`);
+
+    // });
 });
 
 // this is adding numbers to the ID "edit"
- var count = 0;
+var count = 0;
